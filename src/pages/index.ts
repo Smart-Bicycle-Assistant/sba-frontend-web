2efe55947d47776ce9d6cfaf0bc932477b004514
heeeeee0129 <s007kk@ajou.ac.kr>
2023-08-19 23:43:45 +0900

    :sparkles: Add My Bicycle Page (my bicycle list)

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,7 @@
 import MainPage from "./main";
 import RegisterPage from "./register";
 import LoginPage from "./login";
+import BicyclePage from "./bicycle";
+import BicycleDetail from "./bicycleDetail";
 
-export { MainPage, LoginPage, RegisterPage };
+export { MainPage, LoginPage, RegisterPage, BicyclePage, BicycleDetail };
