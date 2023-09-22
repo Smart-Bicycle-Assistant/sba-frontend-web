5d38369ac11a0dcc7cef72a37fd7a9ed6851838a
heeeeee0129 <s007kk@ajou.ac.kr>
2023-09-22 22:19:57 +0900

    :sparkles: Added Terms of Service Page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -6,16 +6,11 @@ function MainPage() {
       <div className="h-auto min-h-screen pb-14">
         <div className="py-4 px-2">
           <div className="flex items-center justify-between">
-            <a href="/register" className="text-blue-500 hover:underline">
+            <a href="/register/terms" className="text-blue-500 hover:underline">
               회원가입
             </a>
             <a href="/login" className="text-blue-500 hover:underline">
               로그인
-            </a>
-          </div>
-          <div>
-            <a href="/mypage" className="text-blue-500 hover:underline">
-              마이페이지
             </a>
           </div>
         </div>
