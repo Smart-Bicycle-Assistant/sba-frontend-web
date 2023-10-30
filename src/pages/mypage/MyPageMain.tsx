c09d5bc47115f7738981bf38db4168449ed5b452
anttiey <hanuuny@gmail.com>
2023-10-30 13:11:37 +0900

    :lipstick: Add My Page Modify UI
    마이페이지 회원 정보 수정 UI 추가

--- src/pages/mypage/MyPageMain.tsx
+++ src/pages/mypage/MyPageMain.tsx
@@ -1,18 +1,18 @@
-import { Link } from "react-router-dom";
-import Header from "../../components/common/Header";
-import Navbar from "../../components/common/Navbar";
+import { Link } from 'react-router-dom';
+import Header from '../../components/common/Header';
+import Navbar from '../../components/common/Navbar';
 
-import { ArrowPathRoundedSquareIcon } from "@heroicons/react/24/outline";
-import { Bars3Icon } from "@heroicons/react/20/solid";
+import { ArrowPathRoundedSquareIcon } from '@heroicons/react/24/outline';
+import { Bars3Icon } from '@heroicons/react/20/solid';
 
 const MyPageMain: React.FC = () => {
   return (
     <div className="content_wrapper">
       <div className="content_fixed">
-        <Header menu={"마이페이지"} />
+        <Header menu={'마이페이지'} />
         <div className="content flex flex-col gap-y-8">
           <div>
-            <p className="pb-3 text-base font-semibold">내 정보</p>
+            <p className="pb-3 text-base font-semibold">마이페이지</p>
             <div className="flex items-center gap-x-3 text-sm">
               <div className="flex justify-center items-center w-16 h-16 bg-neutral-200 rounded-full">
                 <img src="" alt="profile"></img>
@@ -24,7 +24,7 @@ const MyPageMain: React.FC = () => {
             <Link to="/mypage/modify">
               <div className="flex items-center gap-x-3 text-sm px-2 py-5 border-b">
                 <ArrowPathRoundedSquareIcon className="w-5 h-5" />
-                <p>회원 정보 수정</p>
+                <p>회원 정보</p>
               </div>
             </Link>
             <Link to="/mypage/record">
--- src/pages/mypage/MyPageModify.tsx
+++ src/pages/mypage/MyPageModify.tsx
@@ -1,11 +1,48 @@
-import Header from "../../components/common/Header";
-import Navbar from "../../components/common/Navbar";
+import Header from '../../components/common/Header';
+import Navbar from '../../components/common/Navbar';
 
 function MyPageModify() {
   return (
     <div className="h-screen">
       <div className="h-auto min-h-screen pb-14">
-        <Header menu={"내 정보 수정"} />
+        <Header menu={'회원 정보'} />
+        <div className="flex flex-col gap-y-8 px-8 py-8 mx-auto">
+          <div className="p-6 border border-gray-200 rounded-lg">
+            <form className="flex flex-col gap-y-3.5 mb-6">
+              <div>
+                <p className="mb-2 text-xs text-gray-600">이름</p>
+                <input
+                  type="text"
+                  className="w-full text-xs placeholder-slate-400 bg-gray-100 rounded-lg py-3 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                ></input>
+              </div>
+              <div>
+                <p className="mb-2 text-xs text-gray-600">아이디</p>
+                <input
+                  type="text"
+                  className="w-full text-xs placeholder-slate-400 bg-gray-100 rounded-lg py-3 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                ></input>
+              </div>
+              <div>
+                <p className="mb-2 text-xs text-gray-600">이메일</p>
+                <input
+                  type="text"
+                  className="w-full text-xs placeholder-slate-400 bg-gray-100 rounded-lg py-3 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                ></input>
+              </div>
+              <div>
+                <p className="mb-2 text-xs text-gray-600">비밀번호</p>
+                <input
+                  type="text"
+                  className="w-full text-xs placeholder-slate-400 bg-gray-100 rounded-lg py-3 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                ></input>
+              </div>
+            </form>
+            <button className="text-sm w-full bg-customColor text-white py-2.5 mt-3 rounded-lg hover:bg-opacity-80">
+              변경하기
+            </button>
+          </div>
+        </div>
       </div>
       <Navbar />
     </div>
