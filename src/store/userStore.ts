43565a222cbea871fee2453d5da53d6a252841ed
anttiey <hanuuny@gmail.com>
2023-11-02 20:34:13 +0900

    :art: 전역 변수 중복 이름 변경

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,6 +1,6 @@
 // src/store/userStore.ts
 
-import { create } from "zustand";
+import { create } from 'zustand';
 
 interface User {
   id: string;
@@ -11,10 +11,10 @@ interface User {
 }
 
 export const useUser = create<User>((set) => ({
-  id: "",
-  email: "",
-  nickname: "",
-  jwt: "",
+  id: '',
+  email: '',
+  nickname: '',
+  jwt: '',
   setUser: (user: userProps) => {
     set(() => ({
       id: user.id,
