a9de6da0620f8161030a0ed5bfb558799d282bc3
anttiey <hanuuny@gmail.com>
2023-11-06 20:23:22 +0900

    :bug: 커스텀 마커 수정

--- src/components/common/CustomMarker.tsx
+++ src/components/common/CustomMarker.tsx
@@ -4,8 +4,8 @@ import pin from '../../assets/pin.png';
 // Create a custom icon using an image URL
 const CustomMarker = L.icon({
   iconUrl: pin,
-  iconSize: [30, 90],
-  iconAnchor: [16, 32],
+  iconSize: [40, 40],
+  iconAnchor: [0, 0],
 });
 
 export default CustomMarker;
