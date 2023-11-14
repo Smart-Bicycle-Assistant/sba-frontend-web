bb61268fc51a06020b4cbe6944b25ca342dcdaeb
heeeeee0129 <s007kk@ajou.ac.kr>
2023-11-14 13:34:21 +0900

    :green_heart: 빌드 오류 해결

--- src/pages/management/PartManagement.tsx
+++ src/pages/management/PartManagement.tsx
@@ -5,7 +5,7 @@ import usePartState from "../../hooks/usePartState";
 import { BicycleManagementApi, ManagementType } from "../../apis/bicycle";
 import { useState } from "react";
 
-export const PartManagement: React.FC<number> = () => {
+export const PartManagement: React.FC = () => {
   const navigate = useNavigate();
   const { state } = useLocation();
   async function handleUpdate() {
