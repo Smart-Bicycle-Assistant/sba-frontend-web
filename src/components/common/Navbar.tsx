553420129ba10a24f6738d41c47a6926523f6072
heeeeee0129 <s007kk@ajou.ac.kr>
2023-11-21 20:15:28 +0900

    :lipstick: NavBar 아이콘 추가

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,33 +1,54 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useMainBike } from '../../store/userStore';
+import { Link, useNavigate } from "react-router-dom";
+import { useMainBike } from "../../store/userStore";
+import Compass from "../../assets/compass.svg?react";
+import Record from "../../assets/record.svg?react";
+import Setting from "../../assets/setting.svg?react";
+import User from "../../assets/user.svg?react";
+import Home from "../../assets/home.svg?react";
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
   const { main } = useMainBike();
+
+  const navItems = [
+    { to: "/map", label: "주행", icon: <Compass /> },
+    {
+      onClick: () => {
+        console.log(main);
+        navigate("/management", { state: main });
+      },
+      label: "관리",
+      icon: <Setting />,
+    },
+    { to: "/home", label: "홈", icon: <Home /> },
+    { to: "/mypage/record/all", label: "기록", icon: <Record /> },
+    { to: "/mypage", label: "내 정보", icon: <User stroke="#333333" /> },
+  ];
+
   return (
-    <div className="fixed bottom-0 flex w-full text-sm bg-white border-t-2 h-14">
-      <div className="flex flex-col items-center justify-center w-1/4">
-        <Link to="/map">주행</Link>
-      </div>
-      <div className="flex flex-col items-center justify-center w-1/4">
+    <div className="fixed bottom-0 flex w-full text-sm bg-white h-14 border-t-[1px]">
+      {navItems.map((item, index) => (
         <div
-          onClick={() => {
-            console.log(main);
-            navigate('/management', { state: main });
-          }}
+          key={index}
+          className="flex flex-col items-center justify-center w-1/5 pt-2 text-center"
         >
-          관리
+          {item.to ? (
+            <Link to={item.to} className="flex flex-col items-center">
+              {item.icon}
+              <span className="text-[9px] text-gray-600 -mt-1">
+                {item.label}
+              </span>
+            </Link>
+          ) : (
+            <div onClick={item.onClick} className="flex flex-col items-center">
+              {item.icon}
+              <span className="text-[9px] text-gray-600 -mt-1">
+                {item.label}
+              </span>
+            </div>
+          )}
         </div>
-      </div>
-      <div className="flex flex-col items-center justify-center w-1/4">
-        <Link to="/home">홈</Link>
-      </div>
-      <div className="flex flex-col items-center justify-center w-1/4">
-        <Link to="/mypage/record/all">기록</Link>
-      </div>
-      <div className="flex flex-col items-center justify-center w-1/4">
-        <Link to="/mypage">내 정보</Link>
-      </div>
+      ))}
     </div>
   );
 };
