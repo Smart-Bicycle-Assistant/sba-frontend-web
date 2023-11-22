6ab0b8c2ce37f015ca2bb455c6cf5e5f5734f1e0
anttiey <hanuuny@gmail.com>
2023-11-22 21:33:59 +0900

    :bug: 메인 화면 자전거 사진 수정

--- src/components/common/BicycleSwiper.tsx
+++ src/components/common/BicycleSwiper.tsx
@@ -1,4 +1,3 @@
-import Sample from '../../assets/sample.png';
 import { BicycleType } from '../../types';
 
 type BicycleSwiperProps = {
@@ -9,20 +8,20 @@ type BicycleSwiperProps = {
 
 const BicycleSwiper: React.FC<BicycleSwiperProps> = ({ bicycle, activeIndex, index }) => {
   return (
-    <div className="p-8 mx-2 mb-10 bg-white shadow-lg rounded-3xl">
+    <div className="h-[52vh] p-[4.2vh] mx-2 mb-10 bg-white shadow-lg rounded-3xl">
       <div className="flex gap-x-2">
         <div className="flex items-center justify-center w-6 h-6 rounded-full bg-primary-200">
           <p className="text-sm font-semibold text-primary-default">{index + 1}</p>
         </div>
         {activeIndex === index && (
           <div className="flex items-center justify-center px-2 border rounded-full border-rose-500">
-            <p className="text-sm font-semibold text-rose-500">메인</p>
+            <p className="text-sm font-semibold text-rose-500">선택됨</p>
           </div>
         )}
       </div>
-      <div className="py-4 text-xl font-semibold">{bicycle.bicycleName}</div>
-      <div className="flex justify-center py-16">
-        <img src={Sample} alt="main"></img>
+      <div className="py-[2vh] text-xl font-semibold">{bicycle.bicycleName}</div>
+      <div className="h-[33vh] flex items-center justify-center">
+        <img src={bicycle.bicycleImage} alt="main" className="w-full h-[24vh] object-cover"></img>
       </div>
     </div>
   );
