5978f66384cc78ef3bf303fa10286b80d325ad6d
heeeeee0129 <s007kk@ajou.ac.kr>
2023-12-02 15:00:51 +0900

    :bug: Fix a bug

--- src/pages/riding/Riding.tsx
+++ src/pages/riding/Riding.tsx
@@ -213,7 +213,7 @@ const RidingPage: React.FC = () => {
             {state && state.geometry && (
               <Polyline positions={state.geometry} color={"#0064FF"} />
             )}
-            <RecenterAutomatically lat={latitude} lng={longitude} />
+            {/* <RecenterAutomatically lat={latitude} lng={longitude} /> */}
           </MapContainer>
           <div className="absolute top-0 left-1/2 w-1/2 h-screen bg-gradient-to-r from-0% from-transparent to-95% to-primary-400 opacity-50"></div>
           <div className="absolute top-0 flex items-center justify-center w-1/2 h-screen left-1/2">
