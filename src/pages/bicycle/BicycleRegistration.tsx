3c18081e3f840ed06e8ac59484cf8510feeaebf5
heeeeee0129 <s007kk@ajou.ac.kr>
2023-12-06 11:30:25 +0900

    :lipstick: UI 수정

--- src/pages/bicycle/BicycleRegistration.tsx
+++ src/pages/bicycle/BicycleRegistration.tsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
-import Header from '../../components/common/Header';
-import useInput from '../../hooks/useInput';
-import { BicycleManagementApi, BicycleRegistrationApi } from '../../apis/bicycle';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import Header from "../../components/common/Header";
+import useInput from "../../hooks/useInput";
+import {
+  BicycleManagementApi,
+  BicycleRegistrationApi,
+} from "../../apis/bicycle";
+import { useNavigate } from "react-router-dom";
 
 const encodeFileToBase64 = (image: File) => {
   return new Promise((resolve, reject) => {
@@ -18,7 +21,7 @@ const BicycleRegistration: React.FC = () => {
   const { value: name, onChange: onNameChange, setValue: setName } = useInput();
 
   const [image, setImage] = useState<File | null>(null);
-  const [previewImage, setPreviewImage] = useState<string>('');
+  const [previewImage, setPreviewImage] = useState<string>("");
   const [frontLifeSpan, setFrontLifeSpan] = useState<number | null>(null);
   const [rearLifeSpan, setRearLifeSpan] = useState<number | null>(null);
 
@@ -34,7 +37,12 @@ const BicycleRegistration: React.FC = () => {
 
   const onSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    if (name === '' || image === null || frontLifeSpan === null || rearLifeSpan === null) {
+    if (
+      name === "" ||
+      image === null ||
+      frontLifeSpan === null ||
+      rearLifeSpan === null
+    ) {
       return;
     }
 
@@ -61,8 +69,8 @@ const BicycleRegistration: React.FC = () => {
     console.log(tireRes);
 
     if (tireRes.status === 200) {
-      navigate('/bicycle');
-      setName('');
+      navigate("/bicycle");
+      setName("");
     }
   };
 
@@ -82,7 +90,9 @@ const BicycleRegistration: React.FC = () => {
         </div>
 
         <div className="relative h-24 py-5 m-3 bg-white border-2 border-gray-100 rounded-lg">
-          <p className="ml-5 text-sm font-semibold text-black">앞타이어 기대수명</p>
+          <p className="ml-5 text-sm font-semibold text-black">
+            앞타이어 기대수명
+          </p>
           <div className="absolute m-3 right-3 bottom-2">
             <input
               className="w-16 mr-2 text-lg font-bold text-right text-black placeholder-gray-200 bg-transparent border-b border-gray-300"
@@ -97,7 +107,9 @@ const BicycleRegistration: React.FC = () => {
         </div>
 
         <div className="relative h-24 py-5 m-3 bg-white border-2 border-gray-100 rounded-lg">
-          <p className="ml-5 text-sm font-semibold text-black">뒷타이어 기대수명</p>
+          <p className="ml-5 text-sm font-semibold text-black">
+            뒷타이어 기대수명
+          </p>
           <div className="absolute m-3 right-3 bottom-2">
             <input
               className="w-16 mr-2 text-lg font-bold text-right text-black placeholder-gray-200 bg-transparent border-b border-gray-300"
@@ -113,7 +125,9 @@ const BicycleRegistration: React.FC = () => {
 
         <div className="relative h-24 py-5 m-3 bg-white border-2 border-gray-100 rounded-lg">
           <div className="pr-4 mb-6 ">
-            <p className="ml-5 text-sm font-semibold text-black">자전거 이미지</p>
+            <p className="ml-5 text-sm font-semibold text-black">
+              자전거 이미지
+            </p>
             <input
               type="file"
               accept="image/*"
@@ -131,7 +145,7 @@ const BicycleRegistration: React.FC = () => {
           </button>
         </div>
       </div>
-      <div className="text-[10px] fixed bottom-0 p-8 mx-auto text-gray-400">
+      <div className="text-[10px] relative bottom-[-40px] p-8 mx-auto text-gray-400">
         등록한 자전거는 내 자전거 리스트에 표시됩니다.
       </div>
     </div>
--- src/pages/riding/PreRiding.tsx
+++ src/pages/riding/PreRiding.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import Header from '../../components/common/Header';
-import { useNavigate } from 'react-router-dom';
-import PreRidingBox from '../../components/riding/PreRidingBox';
-import { useRidingStore } from '../../store/ridingStore';
-import { useLocationStore } from '../../store/locationStore';
+import { useState } from "react";
+import Header from "../../components/common/Header";
+import { useNavigate } from "react-router-dom";
+import PreRidingBox from "../../components/riding/PreRidingBox";
+import { useRidingStore } from "../../store/ridingStore";
+import { useLocationStore } from "../../store/locationStore";
 
 export const PreRiding: React.FC = () => {
   const [packMode, setpackMode] = useState<boolean>(false);
@@ -25,10 +25,10 @@ export const PreRiding: React.FC = () => {
     if (!(packMode && !targetSpeed)) {
       setRiding({ packMode, targetSpeed, rearDetection, destination });
       if (destination) {
-        navigate('/map');
+        navigate("/map");
       } else {
         setIsRiding(true);
-        navigate('/riding', {
+        navigate("/riding", {
           state: {
             currentCoord: [latitude, longitude],
             startTime: new Date(),
@@ -36,44 +36,46 @@ export const PreRiding: React.FC = () => {
         });
       }
     } else {
-      console.log('목표 속도를 설정하세요');
+      console.log("목표 속도를 설정하세요");
     }
   };
 
   return (
     <div>
-      <Header menu="주행 전 설정" showBackArrow={true} />
-      <PreRidingBox
-        title="팩라이딩"
-        content="목표 속도가 비슷한 주변의 유저들을 주행 중 화면에서 확인할 수 있습니다."
-        state={packMode}
-        onClick={() => setpackMode((prev) => !prev)}
-      />
-      {packMode && (
-        <div className="rounded-lg bg-[#4D93FF] shadow-md py-7 m-4 relative h-32">
-          <p className="ml-5 text-2xl font-bold text-white ">목표속력</p>
-          <div className="absolute m-3 right-3 bottom-2">
-            <input
-              className="mr-2 bg-transparent border-b w-10 border-[#73AAFF] text-right text-white font-bold text-xl"
-              type="number"
-              onChange={handleInputChange}
-            ></input>
-            <span className="text-lg font-bold text-white">km/h</span>
+      <div className="pb-14">
+        <Header menu="주행 전 설정" showBackArrow={true} />
+        <PreRidingBox
+          title="팩라이딩"
+          content="목표 속도가 비슷한 주변의 유저들을 주행 중 화면에서 확인할 수 있습니다."
+          state={packMode}
+          onClick={() => setpackMode((prev) => !prev)}
+        />
+        {packMode && (
+          <div className="rounded-lg bg-[#4D93FF] shadow-md py-7 m-4 relative h-32">
+            <p className="ml-5 text-2xl font-bold text-white ">목표속력</p>
+            <div className="absolute m-3 right-3 bottom-2">
+              <input
+                className="mr-2 bg-transparent border-b w-10 border-[#73AAFF] text-right text-white font-bold text-xl"
+                type="number"
+                onChange={handleInputChange}
+              ></input>
+              <span className="text-lg font-bold text-white">km/h</span>
+            </div>
           </div>
-        </div>
-      )}
-      <PreRidingBox
-        title="후방감지"
-        content="S_BA 확장 어플리케이션을 통해 주행 중 후방의 차량을 감지한 정보를 화면에서 확인할 수 있습니다."
-        state={rearDetection}
-        onClick={() => setRearDetection((prev) => !prev)}
-      />
-      <PreRidingBox
-        title="목적지 설정"
-        content="목적지 설정 후 추천 경로 안내를 받을 수 있습니다."
-        state={destination}
-        onClick={() => setDestination((prev) => !prev)}
-      />
+        )}
+        <PreRidingBox
+          title="후방감지"
+          content="S_BA 확장 어플리케이션을 통해 주행 중 후방의 차량을 감지한 정보를 화면에서 확인할 수 있습니다."
+          state={rearDetection}
+          onClick={() => setRearDetection((prev) => !prev)}
+        />
+        <PreRidingBox
+          title="목적지 설정"
+          content="목적지 설정 후 추천 경로 안내를 받을 수 있습니다."
+          state={destination}
+          onClick={() => setDestination((prev) => !prev)}
+        />
+      </div>
       <div className="fixed bottom-0 left-0 right-0 p-4" onClick={onSubmit}>
         <div className=" text-white py-2.5 px-4 rounded-lg w-full bg-primary-default text-center bg-opacity-85 font-semibold">
           START
