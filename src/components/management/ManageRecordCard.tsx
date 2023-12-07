bfe35523bd1c1c9fa7eccc9bcc95ac3ae90ec2a3
heeeeee0129 <s007kk@ajou.ac.kr>
2023-12-07 20:24:18 +0900

    :recycle: 자전거 최초 입력 시 텍스트 추가

--- src/components/management/ManageRecordCard.tsx
+++ src/components/management/ManageRecordCard.tsx
@@ -8,7 +8,9 @@ const ManageRecordCard = ({
   lifeSpan?: number;
 }) => {
   return (
-    <div className={`flex bg-primary-100 rounded-lg flex-col px-4 py-3 text-sm gap-y-4`}>
+    <div
+      className={`flex bg-primary-100 rounded-lg flex-col px-4 py-3 text-sm gap-y-4`}
+    >
       <div className="flex items-center gap-x-4">
         <div className="w-[73px] px-2 py-1 text-white rounded-lg bg-customColor text-center">
           {part}
@@ -17,8 +19,11 @@ const ManageRecordCard = ({
           {type === 0 && <p className="text-slate-700">해당 없음</p>}
           {type === 1 && <p className="text-green-700">점검</p>}
           {type === 2 && <p className="text-blue-700">교체</p>}
+          {type === 3 && <p className="text-blue-700">최초 입력</p>}
           {type === 2 && lifeSpan && (
-            <p className="text-[10px] text-gray-700 mt-1">(기대 수명: {lifeSpan}km)</p>
+            <p className="text-[10px] text-gray-700 mt-1">
+              (기대 수명: {lifeSpan}km)
+            </p>
           )}
         </div>
       </div>
