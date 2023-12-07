3a724c310499ebc9732a87be3225b1203636b417
anttiey <hanuuny@gmail.com>
2023-12-07 18:44:52 +0900

    :bug: 부품 정보 업데이트 해당 없음 반영

--- src/components/management/ManageRecordCard.tsx
+++ src/components/management/ManageRecordCard.tsx
@@ -7,32 +7,16 @@ const ManageRecordCard = ({
   type: number;
   lifeSpan?: number;
 }) => {
-  let textColor = 'text-gray-700';
-
-  switch (type) {
-    case 1:
-      textColor = 'text-green-700';
-      break;
-    case 2:
-      textColor = 'text-blue-700';
-      break;
-    default:
-      textColor = 'text-black pl-2';
-      break;
-  }
-
   return (
-    <div
-      className={`flex bg-primary-100 rounded-lg flex-col px-4 py-3 text-sm gap-y-4 ${textColor}`}
-    >
+    <div className={`flex bg-primary-100 rounded-lg flex-col px-4 py-3 text-sm gap-y-4`}>
       <div className="flex items-center gap-x-4">
         <div className="w-[73px] px-2 py-1 text-white rounded-lg bg-customColor text-center">
           {part}
         </div>
         <div className="flex items-center gap-2">
-          <p className={`text-${type === 1 ? 'green' : 'blue'}-700`}>
-            {type === 1 ? '점검' : '교체'}
-          </p>
+          {type === 0 && <p className="text-slate-700">해당 없음</p>}
+          {type === 1 && <p className="text-green-700">점검</p>}
+          {type === 2 && <p className="text-blue-700">교체</p>}
           {type === 2 && lifeSpan && (
             <p className="text-[10px] text-gray-700 mt-1">(기대 수명: {lifeSpan}km)</p>
           )}
